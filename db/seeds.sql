e9085c5027aaf2037abda782f37068fc86c91820
michael shamah <shamah.michael@gmail.com>
2016-07-20 13:52:58 -0400

    seeds and schema for the sql

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (user_name, email, address, zipcode) VALUES
+('Trevor', 'trevor@trevor', 'brokyln ny ', 11230 ),
+('mikey', 'mikey@mikey', 'brokyln ny ', 11230 ),
+('Alex', 'alex@alex', 'brokyln ny ', 11230 ),
+('Bobby', 'bobby@bobby', 'brokyln ny ', 11233 ),
+('Jason', 'jason@jason', 'brokyln ny ', 11233 ),
+('Serge', 'serge@serge', 'brokyln ny ', 11233 );
+
+
+
+
+
+
+
+INSERT INTO items(item_name, owner_id, item_desc) VALUES
+('shovel', 1, 'cool shovel check it out' ),
+('shovel', 3, 'cool shovel check it out' ),
+('shovel', 1, 'cool shovel check it out' ),
+('shovel', 4, 'cool shovel check it out' );
+
