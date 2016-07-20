5a05f6e17dbbedb44354a4c8712a510034bbabc3
Trevor Preston <trevorpreston4@gmail.com>
2016-07-20 15:10:22 -0400

    added client jsx files, ajaxAdapter and util.js

--- models/item.js
+++ models/item.js
@@ -9,7 +9,7 @@ const config = {
   port:       process.env.DB_PORT,
   database:   process.env.DB_NAME,
   user:       process.env.DB_USER,
-  password:   process.env.DB_PASS,
+  password:   process.env.DB_PASS
 };
 const db = pg(config);
 
---
+++ src/client/app/App.jsx
@@ -0,0 +1,42 @@
+// import the libs we need
+import React            from 'react'
+import ReactDOM         from 'react-dom'
+
+import ajax             from '../helpers/ajaxAdapter.js'
+import util             from '../helpers/util.js'
+
+
+// create a React Component called _App_
+export default class App extends React.Component{
+  constuctor(){
+    super();
+
+    this.state={
+      user:"",
+      zip: 10025,
+      items: {}
+    }
+  }
+  componentDidMount(){
+    ajax.getItems().then( data=>
+      this.setState({items: data.indexByKey('items_id')})
+    )
+  }
+  addItems(newItem){
+    ajax.addItem(newItem)
+      .then(data=>{
+        this.state.items[ data.item_id ] = data
+        this.setState({tasks: this.state.items})
+      })
+  }
+  render(){
+    return (
+      <h1> Welcome to ShareBear! </h1>
+      <h2> Check out all this awesome shit you borrow. </h2>
+      <ItemList />
+    )
+  }
+}
+
+// mount our App at #container
+ReactDOM.render(<App />, document.querySelector('#container'))
