327e03cbb5727360cb723cf37bf7e01607d55beb
Trevor Preston <trevorpreston4@gmail.com>
2016-07-21 16:06:19 -0400

    reformatted available items to be list items and not buttons.  Added borrow this! button.

--- src/client/app/ItemList.jsx
+++ src/client/app/ItemList.jsx
@@ -5,17 +5,22 @@ export default function ItemList(props) {
 
   return(
     <div className="list-group">
+    <h2> Available Items Near You </h2>
     {console.log(props.list)}
+    <ul>
       {Object.keys(props.list)
         .map(key=>(
-          <button
-            type="button"
-            className="list-group-item"
-            key={key}>
+          <li>
             <strong>{props.list[key].item_name}</strong> {props.list[key].item_desc}
-          </button>
+            <button
+              type="button"
+              key={key}>
+              borrow this!
+            </button>
+          </li>
         ))
       }
+    </ul>
 
     </div>
   )
